a03f1ef10ec3b4d83b7bde311ea281003d314da2
saiman017 <shresthasaiman017@gmail.com>
2022-07-29 22:39:04 +0545

    initial commit

---
+++ Week 11 Exercise/script10.js
@@ -0,0 +1,7 @@
+function calculate()
+         {
+            length = parseInt(prompt("Enter the length of rectangle "));
+            breadth = parseInt(prompt("Enter the breadth of rectangle "));
+            let r = length * breadth;
+            alert("Area of rectangle = "+r);  
+         }
